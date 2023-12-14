83582948760ad8de64febf8f0f949c22639b5fb7
BuildTools <unconfigured@null.spigotmc.org>
2023-12-14 14:31:38 +0100

    Add border styles and print statements

--- src/jdav_badger/prepare_forms.py
+++ src/jdav_badger/prepare_forms.py
@@ -1,12 +1,13 @@
 import csv
 import os
 from openpyxl import load_workbook
+from openpyxl.styles.borders import Border, Side
 
 # input csv file
-INPUT_FILE = 'JLMarken Test.csv'
+INPUT_FILE = 'JLMarken.csv'
 
 # template for output excel file
-TEMPLATE_NAME = 'Jugendleitermarke Bestell Liste'
+TEMPLATE_NAME = 'Jugendleitermarken Bestellliste'
 TEMPLATE_FILE = TEMPLATE_NAME + '.xlsx'
 
 # column indexes in input csv file
@@ -16,7 +17,7 @@ CHAPTER_COLUMN = 2
 BADGE_COLUMN = 3
 
 # row to insert data at
-INSERT_LINE = 5
+INSERT_ROW = 4
 
 # folder to collect output files in
 OUTPUT_FOLDER = 'out'
@@ -30,6 +31,8 @@ badgeMap = {
 }
 
 result = {}
+
+print('Daten werden eingelesen...')
 
 with open(INPUT_FILE, 'r', encoding='cp1252') as file:
     reader = csv.reader(file, delimiter=';')
@@ -59,18 +62,31 @@ with open(INPUT_FILE, 'r', encoding='cp1252') as file:
 if not os.path.isdir(OUTPUT_FOLDER):
     os.mkdir(OUTPUT_FOLDER)
 
+# create border
+sideThin = Side(style=None, color='FF000000', border_style='thin')
+border = Border(left=sideThin, right=sideThin, bottom=sideThin, top=sideThin)
+
+print('Dateien werden erstellt...')
+
 for chapter in result.keys():
+    print('Aktuelle Sektion: ' + chapter, end="\x1b[1K\r")
+
     # open worksheet
     workbook = load_workbook(TEMPLATE_FILE)
     worksheet = workbook.active
 
     # insert sufficient rows
-    worksheet.insert_rows(INSERT_LINE, amount=len(result[chapter]))
+    worksheet.insert_rows(INSERT_ROW, amount=len(result[chapter]))
 
     # write cells
-    for row_index, row_data in enumerate(result[chapter], start=INSERT_LINE):
+    for row_index, row_data in enumerate(result[chapter], start=INSERT_ROW):
         for col_index, cell_value in enumerate(row_data, start=1):
             worksheet.cell(row=row_index, column=col_index).value = cell_value
 
+        for i in range(8):
+            worksheet.cell(row=row_index, column=i+1).border = border
+
     # save result to output folder
     workbook.save(OUTPUT_FOLDER + '/' + TEMPLATE_NAME + ' ' + chapter + '.xlsx')
+
+print('Vorgang abgeschlossen!')
